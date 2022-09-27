cb361da481d77464e49fb8e3db614920ce02ebf2
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2022-09-27 23:11:39 +0300

    removed BenchmarkMattress

--- README.md
+++ README.md
@@ -4,16 +4,16 @@ Float Air Mattress is a container for a slice that could be used with [sync.Pool
 Has a minimum allocations.
 
 ## Benchmarks
-BenchmarkMattress is kinda unreal situation, but BenchmarkPool is.
 ```shell
 go test -bench=. -cpu=4 -benchmem
 goos: darwin
 goarch: amd64
 pkg: github.com/vtopc/go-airmat
 cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
-BenchmarkMakeSlices-4   	  400750	      2688 ns/op	   16368 B/op	      10 allocs/op
-BenchmarkMattress-4     	10026760	       120.1 ns/op	       0 B/op	       0 allocs/op
-BenchmarkPool/8-4       	 4274638	       302.6 ns/op	       0 B/op	       0 allocs/op
-BenchmarkPool/16-4      	   59446	     20567 ns/op	      30 B/op	       0 allocs/op
-BenchmarkPool/24-4      	     100	  11741976 ns/op	 4023936 B/op	       0 allocs/op
+BenchmarkMakeSlices/8-4 	 1677363	       731.3 ns/op	    4080 B/op	       8 allocs/op
+BenchmarkMakeSlices/16-4         	   10000	    105324 ns/op	 1048563 B/op	      16 allocs/op
+BenchmarkMakeSlices/24-4         	     100	  15483531 ns/op	268435443 B/op	      24 allocs/op
+BenchmarkPool/8-4                	 4156912	       288.8 ns/op	       0 B/op	       0 allocs/op
+BenchmarkPool/16-4               	   56299	     19531 ns/op	       9 B/op	       0 allocs/op
+BenchmarkPool/24-4               	     133	   8669618 ns/op	      13 B/op	       0 allocs/op
 ```
--- bench_test.go
+++ bench_test.go
@@ -7,6 +7,8 @@ import (
 	"github.com/vtopc/go-airmat"
 )
 
+var parallelRequests = []int{8, 16, 24}
+
 // Global variable for Benchmarking - https://itnext.io/the-top-10-most-common-mistakes-ive-seen-in-go-projects-4b79d4f6cd65
 var (
 	m    = &airmat.Mattress[int]{}
@@ -15,37 +17,32 @@ var (
 )
 
 func BenchmarkMakeSlices(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		for i := 0; i < 10; i++ {
-			v := make([]int, 2<<i)
-			s = v
-		}
-	}
-}
-
-func BenchmarkMattress(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		for i := 0; i < 10; i++ {
-			m.SetSize(2 << i)
-		}
+	for _, requests := range parallelRequests {
+		b.Run(strconv.Itoa(requests), func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				for i := 0; i < requests; i++ {
+					v := make([]int, 2<<i)
+					s = v
+				}
+			}
+		})
 	}
 }
 
 func BenchmarkPool(b *testing.B) {
-	poolSizes := []int{8, 16, 24}
-
-	for _, poolSize := range poolSizes {
-		b.Run(strconv.Itoa(poolSize), func(b *testing.B) {
-			mm := make([]*airmat.Mattress[int], poolSize)
+	for _, requests := range parallelRequests {
+		b.Run(strconv.Itoa(requests), func(b *testing.B) {
+			mm := make([]*airmat.Mattress[int], requests)
 
 			for n := 0; n < b.N; n++ {
-				for i := 0; i < poolSize; i++ {
+				// emulates parallel requests
+				for i := 0; i < requests; i++ {
 					m := pool.Get()
 					m.SetSize(2 << i)
 					mm[i] = m
 				}
 
-				for i := 0; i < poolSize; i++ {
+				for i := 0; i < requests; i++ {
 					pool.Put(mm[i])
 				}
 			}
